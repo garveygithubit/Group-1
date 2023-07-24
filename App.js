0a321a656da3b28497d9cd851a73a3d3fea82f70
Nana Kwame <iabu007@st.ug.edu.gh>
2023-07-24 02:06:36 +0000

    accepted incoming changes

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
-
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import MapScreen from "./screens/MapScreen";
-import HotelDetails from './views/hotel_details'
+import HotelDetails from "./views/hotel_details";
 
 export default function App() {
   return (
     <>
-<<<<<<< HEAD
-=======
-    
->>>>>>> 423471b548ee07a8253e615acd4c19c3654a999f
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
